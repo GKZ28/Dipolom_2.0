856aafe771890be7a8b51aa0b5a11e8227941755
gkz <giorgadzekonstantin@gmail.com>
2024-03-03 16:59:45 +0300

    Update SQLHelper added @Sneaky Throws

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -1,5 +1,6 @@
 package ru.netology.data;
 
+import lombok.SneakyThrows;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 
@@ -12,52 +13,37 @@ public class SQLHelper {
     private static final String USERNAME = System.getProperty("db.username");
     private static final String PASSWORD = System.getProperty("db.password");
 
-    private static Connection startConnection() {
-        Connection connect = null;
-        try {
-            connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        }
-        return connect;
+    private static Connection startConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 
+    @SneakyThrows(SQLException.class)
     public static String statusOfPayment() {
         QueryRunner runner = new QueryRunner();
         String payStatus = "SELECT status FROM payment_entity";
-
         try (Connection connect = startConnection()) {
             var paymentStatus = runner.query(connect, payStatus, new BeanHandler<>(Debit.class));
             return paymentStatus.getStatus();
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
         }
-        return null;
     }
 
+    @SneakyThrows(SQLException.class)
     public static String statusOfCredit() {
         QueryRunner runner = new QueryRunner();
         String cStatus = "SELECT status FROM credit_request_entity";
-
         try (Connection connect = startConnection()) {
             var creditStatus = runner.query(connect, cStatus, new BeanHandler<>(Credit.class));
             return creditStatus.getStatus();
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
         }
-        return null;
     }
 
+    @SneakyThrows(SQLException.class)
     public static String statusOfAmount() {
         QueryRunner runner = new QueryRunner();
         String payAmount = "SELECT amount FROM payment_entity";
-
         try (Connection connect = startConnection()) {
             var paymentAmount = runner.query(connect, payAmount, new BeanHandler<>(Debit.class));
             return paymentAmount.getAmount();
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
         }
-        return null;
     }
 }

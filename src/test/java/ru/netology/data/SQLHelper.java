0bd6820ca63c181dcd452eb36e1a603fcd0f7f26
gkz <giorgadzekonstantin@gmail.com>
2024-03-03 16:50:25 +0300

    update sqlhelper (added static methods)

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -7,14 +7,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class SQLHelper {
     private static final String URL = System.getProperty("db.url");
     private static final String USERNAME = System.getProperty("db.username");
     private static final String PASSWORD = System.getProperty("db.password");
-    private static Connection connect;
 
     private static Connection startConnection() {
+        Connection connect = null;
         try {
             connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException sqlException) {
@@ -23,11 +22,11 @@ public class SQLHelper {
         return connect;
     }
 
-    public String statusOfPayment() {
-        var runner = new QueryRunner();
-        var payStatus = "SELECT status FROM payment_entity";
+    public static String statusOfPayment() {
+        QueryRunner runner = new QueryRunner();
+        String payStatus = "SELECT status FROM payment_entity";
 
-        try (var connect = startConnection()) {
+        try (Connection connect = startConnection()) {
             var paymentStatus = runner.query(connect, payStatus, new BeanHandler<>(Debit.class));
             return paymentStatus.getStatus();
         } catch (SQLException sqlException) {
@@ -36,11 +35,11 @@ public class SQLHelper {
         return null;
     }
 
-    public String statusOfCredit() {
-        var runner = new QueryRunner();
-        var cStatus = "SELECT status FROM credit_request_entity";
+    public static String statusOfCredit() {
+        QueryRunner runner = new QueryRunner();
+        String cStatus = "SELECT status FROM credit_request_entity";
 
-        try (var connect = startConnection()) {
+        try (Connection connect = startConnection()) {
             var creditStatus = runner.query(connect, cStatus, new BeanHandler<>(Credit.class));
             return creditStatus.getStatus();
         } catch (SQLException sqlException) {
@@ -49,11 +48,11 @@ public class SQLHelper {
         return null;
     }
 
-    public String statusOfAmount() {
-        var runner = new QueryRunner();
-        var payAmount = "SELECT amount FROM payment_entity";
+    public static String statusOfAmount() {
+        QueryRunner runner = new QueryRunner();
+        String payAmount = "SELECT amount FROM payment_entity";
 
-        try (var connect = startConnection()) {
+        try (Connection connect = startConnection()) {
             var paymentAmount = runner.query(connect, payAmount, new BeanHandler<>(Debit.class));
             return paymentAmount.getAmount();
         } catch (SQLException sqlException) {
